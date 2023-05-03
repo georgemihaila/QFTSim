1f2b0de40e298628639ad9972c007cfd59b96944
George <george@cce.sh>
2023-05-03 11:02:15 +0300

    Coob rotates

--- qft-sim/src/scenes/CoobScene.tsx
+++ qft-sim/src/scenes/CoobScene.tsx
@@ -1,26 +1,29 @@
 import { useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { LUTPass, LUTCubeLoader } from 'three-stdlib'
-import { useTexture } from '@react-three/drei';
+import { useRef } from 'react';
 import { Vector3 } from 'three';
 import { Coob } from '../physics';
+import { animated } from '@react-spring/three'
+import { useTexture } from '@react-three/drei';
 
 export function CoobScene() {
-    const ref = useRef<any>()
+    const meshRef = useRef<THREE.Mesh>(null);
     const coob = new Coob(new Vector3(0, 0.5, 0));
-    coob.ref = ref.current
-    useFrame(({ clock }) => {
+    coob.ref = meshRef.current
+
+    // Update the mesh rotation based on the spring value
+    useFrame(() => {
         coob.tick()
-    })
-
+        if (meshRef.current) {
+            meshRef.current.rotation.y = coob.rotation.y;
+            meshRef.current.rotation.x = coob.rotation.x;
+            meshRef.current.rotation.z = coob.rotation.z;
+        }
+    });
     const texture = useTexture('/terrazo.png')
-
     return (
-        <>
-            <mesh ref={ref}>
-                <boxGeometry />
-                <meshPhysicalMaterial map={texture} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.8} />
-            </mesh>
-        </>
+        <animated.mesh position={coob.position} ref={meshRef}>
+            <boxBufferGeometry />
+            <meshStandardMaterial map={texture} roughness={0} metalness={0.8} />
+        </animated.mesh>
     );
 }
